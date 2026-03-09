a6e037938adc0a5b02b4e25212d30b1fcaf6467e
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-03-08 21:42:56 -0300

    Cria o projeto

---
+++ jogadores/src/main/java/net/danielaraujo/jogadores/JogadoresApplication.java
@@ -0,0 +1,13 @@
+package net.danielaraujo.jogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogadoresApplication.class, args);
+	}
+
+}
---
+++ jogadores/src/main/java/net/danielaraujo/jogadores/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.danielaraujo.jogadores;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JogadoresApplication.class);
+	}
+
+}
